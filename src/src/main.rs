b7e6858bfceea3ee2f69032949da0d91ce8a6030
ZlayaMorda <tarus01@mail.ru>
2024-03-26 12:24:11 +0100

    add clap and args for cli

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// period for message sending in seconds
+    #[arg(long, default_value_t = 1)]
+    period: u32,
+    /// Port where peer starts
+    #[arg(long)]
+    port: String,
+    /// Socket address to connect
+    #[arg(short, long)]
+    connect: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello {:?}", args);
 }
